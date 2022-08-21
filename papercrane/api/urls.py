3f965921b2920d5ccaf32385d2b631eec6ad1ea6
surzia <songjie19970916@163.com>
2022-08-21 10:53:40 +0800

    feat:implement api design

--- papercrane/api/urls.py
+++ papercrane/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from api.views import CreatePostView, ListPostView, CreatePost
+from api.views import CreatePostView, ListPostView, CreatePost, ViewPost
 
 urlpatterns = [
     path('create', CreatePostView.as_view()),
     path('list', ListPostView.as_view()),
     path('post', CreatePost.as_view()),
+    path('view/<int:pk>', ViewPost.as_view()),
 ]
