25c44dbd18990959918885800b8233053942de31
surzia <songjie19970916@163.com>
2022-08-27 23:59:26 +0800

    feat:move post list into card in home page

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
-import List from "./pages/List";
 import Paper from "./pages/Paper";
 
 class App extends Component {
@@ -9,7 +8,6 @@ class App extends Component {
     return (
       <Router>
         <Switch>
-          <Route path="/list" component={List} />
           <Route path="/page" component={Paper} />
           <Route path="/" component={Home} />
         </Switch>
--- web/src/pages/Home.js
+++ web/src/pages/Home.js
@@ -8,11 +8,39 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Link from "@mui/material/Link";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Grid from "@mui/material/Grid";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme({});
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: [],
+      total: 0,
+      page: 1,
+      size: 10,
+    };
+  }
+
+  componentDidMount() {
+    fetch(
+      `http://localhost:8000/api/list?page=${this.state.page}&size=${this.state.size}`
+    )
+      .then((r) => r.json())
+      .then((data) => {
+        this.setState({
+          total: data.count,
+          cards: data.results,
+        });
+      });
+  }
+
   render() {
     return (
       <ThemeProvider theme={theme}>
@@ -62,7 +90,45 @@ class App extends Component {
               </Stack>
             </Container>
           </Box>
-          <Container sx={{ py: 8 }} maxWidth="md"></Container>
+          <Container sx={{ py: 8 }} maxWidth="md">
+            {/* End hero unit */}
+            <Grid container spacing={4}>
+              {this.state.cards.map((card) => (
+                <Grid item key={card} xs={12} sm={6} md={4}>
+                  <Card
+                    sx={{
+                      height: "100%",
+                      display: "flex",
+                      flexDirection: "column",
+                    }}
+                  >
+                    <CardMedia
+                      component="img"
+                      sx={{
+                        // 16:9
+                        pt: "56.25%",
+                      }}
+                      image="https://source.unsplash.com/random"
+                      alt="random"
+                    />
+                    <CardContent sx={{ flexGrow: 1 }}>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        Heading
+                      </Typography>
+                      <Typography>
+                        This is a media card. You can use this section to
+                        describe the content.
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small">View</Button>
+                      <Button size="small">Edit</Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
         </main>
         {/* Footer */}
         <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
