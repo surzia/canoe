0b8b05f44a52ec1c443eacec672db4994f1b7ed0
habsburger <1423255902@qq.com>
2023-01-11 21:55:58 +0800

    feat:support sort by created time in home page

--- crane-be/api/story.go
+++ crane-be/api/story.go
@@ -28,6 +28,7 @@ func (s *Server) CreateStory(c *gin.Context) {
 func (s *Server) QueryStories(c *gin.Context) {
 	page := c.Query("page")
 	size := c.Query("size")
+	sort := c.Query("sort")
 
 	var pageNum, pageSize int
 	if page == "" {
@@ -42,8 +43,12 @@ func (s *Server) QueryStories(c *gin.Context) {
 		pageSize, _ = strconv.Atoi(size)
 	}
 
+	if sort == "" {
+		sort = "desc"
+	}
+
 	storyService := services.NewStoryService(s.db)
-	stories := storyService.QueryStories(pageNum, pageSize)
+	stories := storyService.QueryStories(pageNum, pageSize, sort)
 	count := storyService.CountStories()
 
 	ret := make(map[string]interface{})
--- crane-fe/src/components/Feed.tsx
+++ crane-fe/src/components/Feed.tsx
@@ -8,9 +8,12 @@ import {
   CardContent,
   Divider,
   Grid,
+  IconButton,
   Pagination,
   Typography,
 } from "@mui/material";
+import KeyboardControlKeyIcon from "@mui/icons-material/KeyboardControlKey";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 import { BACKEND_API_HOST, goto } from "../common";
 import StoryBoard from "./StoryBoard";
@@ -19,27 +22,49 @@ function Feed() {
   const [page, setPage] = React.useState<number>(1);
   const [size, setSize] = React.useState<number>(10);
   const [count, setCount] = React.useState<number>(0);
+  const [sort, setSort] = React.useState<string>("desc");
   const [stories, setStories] = React.useState<Story[]>([]);
 
   React.useEffect(() => {
-    fetch(`${BACKEND_API_HOST}/story/query?page=${page}&size=${size}`)
+    fetchStoryFeed();
+  }, [page, size]);
+
+  const fetchStoryFeed = () => {
+    fetch(
+      `${BACKEND_API_HOST}/story/query?page=${page}&size=${size}&sort=${sort}`
+    )
       .then((r) => r.json())
       .then((data) => {
         setStories(data.data.stories);
         setCount(data.data.count);
         setSize(10);
       });
-  }, [page, size]);
+  };
 
   const pageChange = (_event: React.ChangeEvent<unknown>, value: number) => {
     setPage(value);
   };
 
+  const sortStory = () => {
+    let t = sort === "desc" ? "asc" : "desc";
+    setSort(t);
+    fetchStoryFeed();
+  };
+
   return (
     <Grid item xs={12} md={8}>
-      <Typography variant="h6" gutterBottom>
-        Your stories
-      </Typography>
+      <Box display="flex">
+        <Typography variant="h6" gutterBottom sx={{ flex: 1 }}>
+          Your stories
+        </Typography>
+        <IconButton onClick={sortStory}>
+          {sort === "desc" ? (
+            <KeyboardArrowDownIcon />
+          ) : (
+            <KeyboardControlKeyIcon />
+          )}
+        </IconButton>
+      </Box>
       <Divider />
       {stories.map((story) => (
         <Card key={story.sid} variant="outlined" sx={{ m: 2 }}>
