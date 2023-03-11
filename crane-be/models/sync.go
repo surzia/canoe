8d4ff0b2d48d33e91c334d295442cb924336d089
surzia <songjie19970916@163.com>
2023-03-11 19:49:38 +0800

    feat:add sync table

--- crane-be/models/sync.go
+++ crane-be/models/sync.go
@@ -1,5 +1,14 @@
 package models
 
+import "gorm.io/gorm"
+
+type Sync struct {
+	gorm.Model
+	Type     string `json:"type"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 type JianGuoConnectReq struct {
 	User     string `json:"user"`
 	Password string `json:"password"`
