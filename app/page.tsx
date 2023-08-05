c3f83f3d76364c4f29893b5e217d0a0208527b95
surzia <surzia@163.com>
2023-08-05 21:39:41 +0800

    feat:first design of papercrane prototype

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,69 @@
-import NextLink from "next/link";
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code"
-import { button as buttonStyles } from "@nextui-org/theme";
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import { Card, CardBody } from "@nextui-org/card";
+import { Pagination } from "@nextui-org/pagination";
 
 export default function Home() {
-	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>Make&nbsp;</h1>
-				<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-				<br />
-				<h1 className={title()}>
-					websites regardless of your design experience.
-				</h1>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Beautiful, fast and modern React UI library.
-				</h2>
-			</div>
-
-			<div className="flex gap-3">
-				<Link
-					isExternal
-					as={NextLink}
-					href={siteConfig.links.docs}
-					className={buttonStyles({ color: "primary", radius: "full", variant: "shadow" })}
-				>
-					Documentation
-				</Link>
-				<Link
-					isExternal
-					as={NextLink}
-					className={buttonStyles({ variant: "bordered", radius: "full" })}
-					href={siteConfig.links.github}
-				>
-					<GithubIcon size={20} />
-					GitHub
-				</Link>
-			</div>
-
-			<div className="mt-8">
-				<Snippet hideSymbol hideCopyButton variant="flat">
-					<span>
-						Get started by editing <Code color="primary">app/page.tsx</Code>
-					</span>
-				</Snippet>
-			</div>
-		</section>
-	);
+  return (
+    <>
+      <div className="grid grid-cols-3 gap-4">
+        <Card className="my-1">
+          <CardBody>
+            <p>Make beautiful websites regardless of your design experience.</p>
+          </CardBody>
+        </Card>
+        <Card className="my-1">
+          <CardBody>
+            <p>Make beautiful websites regardless of your design experience.</p>
+          </CardBody>
+        </Card>
+        <Card className="my-1">
+          <CardBody>
+            <p>Make beautiful websites regardless of your design experience.</p>
+          </CardBody>
+        </Card>
+        <Card className="my-1">
+          <CardBody>
+            <p>Make beautiful websites regardless of your design experience.</p>
+            <p>Make beautiful websites regardless of your design experience.</p>
+            <p>Make beautiful websites regardless of your design experience.</p>
+            <p>Make beautiful websites regardless of your design experience.</p>
+            <p>Make beautiful websites regardless of your design experience.</p>
+            <p>Make beautiful websites regardless of your design experience.</p>
+          </CardBody>
+        </Card>
+        <Card className="my-1">
+          <CardBody>
+            <p>Make beautiful websites regardless of your design experience.</p>
+          </CardBody>
+        </Card>
+        <Card className="my-1">
+          <CardBody>
+            <p>Make beautiful websites regardless of your design experience.</p>
+          </CardBody>
+        </Card>
+        <Card className="my-1">
+          <CardBody>
+            <p>Make beautiful websites regardless of your design experience.</p>
+          </CardBody>
+        </Card>
+        <Card className="my-1">
+          <CardBody>
+            <p>Make beautiful websites regardless of your design experience.</p>
+          </CardBody>
+        </Card>
+        <Card className="my-1">
+          <CardBody>
+            <p>Make beautiful websites regardless of your design experience.</p>
+          </CardBody>
+        </Card>
+        <Card className="my-1">
+          <CardBody>
+            <p>Make beautiful websites regardless of your design experience.</p>
+          </CardBody>
+        </Card>
+      </div>
+      <div className="flex justify-center">
+        <Pagination total={10} initialPage={1} />
+      </div>
+    </>
+  );
 }
