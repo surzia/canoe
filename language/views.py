2d11622257befbc82ce1873b958d974693ce3844
twelve <1423255902@qq.com>
2024-05-12 22:11:48 +0800

    add search word feature

--- language/views.py
+++ language/views.py
@@ -4,12 +4,17 @@ from .models import Language
 
 
 def word_list(request):
-    words = Language.objects.all().order_by('id')
+    query = request.GET.get('s')
+    if query is None:
+        words = Language.objects.all().order_by('id')
+    else:
+        words = Language.objects.filter(word__contains=query).order_by('id')
     per_page = 10
     paginator = Paginator(words, per_page)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
         'words': page_obj,
+        'query': query,
     }
     return render(request, 'language.html', context)
