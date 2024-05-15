45ee8fbf3428af19128e21ec5323acdd5aafa500
twelve <1423255902@qq.com>
2024-05-15 20:37:23 +0800

    support shenlun answer

--- argumentation/urls.py
+++ argumentation/urls.py
@@ -5,4 +5,6 @@ from argumentation import views
 urlpatterns = [
     path('list', views.display_all_exams, name='argumentation'),
     path('exam/<int:exam_id>', views.display_exam, name='exam'),
+    path('answer/<int:exam_id>/<int:read_id>', views.answer, name='answer'),
+    path('answer/submit', views.submit_exam, name='submit'),
 ]
--- argumentation/views.py
+++ argumentation/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+import json
 
-from argumentation.models import Exam, Reading, Paragraph
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
+from argumentation.models import Exam, Reading, Paragraph, Answer
 
 
 def display_all_exams(request):
@@ -22,3 +26,34 @@ def display_exam(request, exam_id):
         'paragraph': paragraph
     }
     return render(request, 'detail.html', context)
+
+
+def answer(request, exam_id, read_id):
+    exam = Exam.objects.get(pk=exam_id)
+    read = Reading.objects.get(exam=exam, code=read_id)
+    paragraph = Paragraph.objects.filter(reading=read)
+    context = {
+        'exam': exam,
+        'read': read,
+        'paragraph': paragraph
+    }
+    return render(request, 'answer.html', context)
+
+
+@csrf_exempt
+def submit_exam(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        exam_id = data['exam_id']
+        read_id = data['read_id']
+        exam = Exam.objects.get(pk=exam_id)
+        read = Reading.objects.get(exam=exam, code=read_id)
+        answer_text = data['answer']
+        word_count = len(answer_text.strip())
+        ans = Answer(reading=read, answer=answer_text, word_count=word_count)
+        ans.save()
+        try:
+            n = Reading.objects.get(exam=exam, code=read_id + 1)
+            return JsonResponse({'id': n.code})
+        except Reading.DoesNotExist:
+            return JsonResponse({'id': -1})
