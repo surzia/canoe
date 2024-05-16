a0eda334e9ed5a025126960871f212d6106d39b1
twelve <1423255902@qq.com>
2024-05-16 21:58:50 +0800

    is shenlun completed

--- argumentation/models.py
+++ argumentation/models.py
@@ -8,6 +8,25 @@ class Exam(models.Model):
     def __str__(self):
         return self.name
 
+    def is_complete(self):
+        """
+        1: completed
+        0: not completed
+        -1 unread
+        :return: -1/0/1
+        """
+        reads = self.reading_set.all()
+        status = -1
+        flag = True
+        for read in reads:
+            if read.has_answer():
+                status = 0
+            else:
+                flag = False
+        if flag:
+            return -1
+        return status
+
 
 class Reading(models.Model):
     exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
@@ -18,6 +37,10 @@ class Reading(models.Model):
 
     def __str__(self):
         return '{} 材料{}'.format(self.exam.name, self.code)
+
+    def has_answer(self):
+        ans = self.answer_set.all()
+        return len(ans) > 0
 
 
 class Paragraph(models.Model):
